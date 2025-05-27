283ee793e8007d8f01cfa336fa336e056e486ce1
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-05-27 22:21:39 +0530

    feature pregnancystages

--- client/lib/core/constants/api_contants.dart
+++ client/lib/core/constants/api_contants.dart
@@ -1,7 +1,8 @@
 class ApiContants {
 
     // static const baseUrl = 'http://10.0.2.2:5003'; // for emulator
-    static const baseUrl = 'http://192.168.151.198:5003'; // for phone
+    static const baseUrl = 'http://10.0.2.2:8000'; // for emulatorfastapi
+    // static const baseUrl = 'http://192.168.151.198:5003'; // for phone
 
 
 }
